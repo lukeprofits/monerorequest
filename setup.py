e5570f5f939d9a7bcdf0339d21265399f582c629
Luke Profits <38372395+lukeprofits@users.noreply.github.com>
2023-06-08 02:26:13 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='monerosub',
+    version='1.0.0',
+    author="Luke Profits",
+    description="monerosub is an easy way to create/decode monero-subscription codes.",
+    url="https://github.com/lukeprofits/monerosub",
+    packages=['monerosub'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
